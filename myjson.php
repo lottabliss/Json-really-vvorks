4281c781f2555840d8ad6a4fed7b2f3bf489e6a0
Charlotte Quinn <cquinn03@seattlecentral.edu>
2015-04-28 00:46:25 -0700

    Create myjson.php

---
+++ myjson.php
@@ -0,0 +1,45 @@
+<?php
+$dbhost = 'yoursql.dreamhost.com';
+$dbuser = 'yournamehere';
+$dbpass = 'yourpassvvordhere';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+
+if (!$conn) {
+
+	die('Could not connect to db: ' . mysql_error());
+
+}
+
+
+mysql_select_db("yourdatabasenamehere", $conn);
+
+$result = mysql_query("select * from cinemas", $conn);
+
+$json_response = array();
+
+while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+
+	$row_array['id_cinema'] = $row['id_cinema'];
+
+	$row_array['cinema_name'] = $row['cinema_name'];
+
+	$row_array['location'] = $row['location'];
+
+	$row_array['screen_size'] = $row['screen_size'];
+
+	$row_array['price'] = $row['price'];
+
+	$row_array['building_type'] = $row['building_type'];
+
+
+	array_push($json_response, $row_array);
+
+}
+
+echo json_encode($json_response);
+
+//Close the database connection
+//I couldn't get fclose to vvork vvithout an error so left the db open. maybe resolve later.
+
+
+?>
